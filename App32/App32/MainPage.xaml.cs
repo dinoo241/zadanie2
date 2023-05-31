ec94fba2b35132c6571a3b6e3b8269f14d2b425b
dinoo241 <kubamikolajczyk12345@gmail.com>
2023-05-31 22:48:06 +0200

    Sample commit

---
+++ App32/App32/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using App32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using System.Collections;
+using Xamarin.Essentials;
+using System.Net.Http;
+
+namespace App32
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            data();
+        }
+        private List<Repository> sList;
+        private async void data()
+        {
+            using (var client = new HttpClient())
+            {
+                var connect = await client.GetAsync("http://10.0.2.2:3000/data");
+                string con = await connect.Content.ReadAsStringAsync();
+                sList = JsonConvert.DeserializeObject<List<Repository>>(con);
+            }
+            for (int i = 0; i < sList.Count; i++)
+            {
+                chooseStation.Items.Add(sList[i].Stacja);
+            }
+        }
+        private void stacjaINFO(object sender, EventArgs e)
+        {
+            Repository repo = sList[chooseStation.SelectedIndex];
+
+            nStacja.Text = $"Nazwa stacji: {repo.Stacja}";
+            nId.Text = $"id stacji: {repo.Id_stacji}";
+            nData.Text = $"Data pomiaru: {repo.Data_pomiaru}";
+            nTemperatura.Text = $"Temperatura: {repo.Temperatura} ℃";
+            nPredkosc.Text = $"Predkosc wiatru wynosi: {repo.Predkosc_wiatru} km/h";
+            nKierunek.Text = $"Kierunek wiatru to: {repo.Kierunek_wiatru}";
+            nSuma.Text = $"Suma opadow to: {repo.Temperatura}";
+            nCisnienie.Text = $"Cisnienie wynosi: {repo.Cisnienie} hPa";
+            nWilgotnosc.Text = $"Wilgotnosc wzgledna wynosi: {repo.Wilgotnosc_wzgledna} %";
+
+
+        }
+    }
+}
---
+++ App32/App32/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace App32
+{
+    public class Repository
+    {
+        [JsonProperty("stacja")]
+        public string Stacja { get; set; }
+
+        [JsonProperty("id_stacji")]
+        public string Id_stacji { get; set; }
+
+        [JsonProperty("data_pomiaru")]
+        public string Data_pomiaru { get; set; }
+
+        [JsonProperty("temperatura")]
+        public string Temperatura { get; set; }
+
+        [JsonProperty("predkosc_wiatru")]
+        public string Predkosc_wiatru { get; set; }
+
+        [JsonProperty("kierunek_wiatru")]
+        public string Kierunek_wiatru { get; set; }
+
+        [JsonProperty("suma_opadu")]
+        public string Suma_opadu { get; set; }
+
+        [JsonProperty("cisnienie")]
+        public string Cisnienie { get; set; }
+
+        [JsonProperty("wilgotnosc_wzgledna")]
+        public string Wilgotnosc_wzgledna { get; set; }
+    }
+}
